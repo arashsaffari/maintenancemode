52c6b81beccfdb4645a942a088e951f6c8ec0ca7
Arash Saffari <arash@saffari.com>
2019-05-26 20:50:17 +0200

    add german language

---
+++ src/Language/de/MaintenanceMode.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+	'serverDowTitle'	=> "503 - Dienst nicht verfügbar",
+	'serverDowMessage'	=> "Es tut uns leid! Wir sind damit beschäftigt, den Server für Sie zu aktualisieren und werden bald wieder Online.",
+];
